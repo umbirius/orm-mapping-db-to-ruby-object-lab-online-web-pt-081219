1d11432d9dfea83dabe3088261fd0c5166cfa849
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-18 18:07:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,6 +15,19 @@ class Student
     # remember each row should be a new instance of the Student class
   end
   
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * 
+      FROM students 
+      WHERE grade < 12
+    SQL
+    
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+    
+  end 
+  
   def self.all_students_in_grade_9 
     sql = <<-SQL
       SELECT * 
