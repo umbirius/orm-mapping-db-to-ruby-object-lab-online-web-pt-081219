4c0189cbde018358beab2de027a78d5b75bb3072
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-18 18:12:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,6 +10,19 @@ class Student
     new_student
   end
 
+  def self.all_students_in_grade_x(grade)
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE grade = 
+    SQL
+    
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end.first(x)
+  end 
+
+
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT * 
