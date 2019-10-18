f50fe4d9b4fa8f4f0d8207503b6b67b681e71842
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-18 18:02:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,6 +14,12 @@ class Student
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
+  
+  def self.all_students_in_grade_9 
+    sql = <<-SQL SELECT * 
+    FROM students 
+    WHERE grade = 9 
+  end 
 
   def self.find_by_name(name)
     # find the student in the database given a name
