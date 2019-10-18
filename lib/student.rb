ed3aa2b16cf202a4a98b3398d1863bfafef57afa
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-18 18:13:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
     new_student
   end
 
-  def self.all_students_in_grade_x(grade)
+  def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT * 
       FROM students
