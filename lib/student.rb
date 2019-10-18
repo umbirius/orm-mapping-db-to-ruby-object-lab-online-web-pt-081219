a5252eb8465b1b06a54c088cdabc1583f4c9cdb1
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-18 18:01:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
-    sql <<-SQL 
+    sql = <<-SQL 
       SELECT *
       FROM students
       WHERE name = ?
