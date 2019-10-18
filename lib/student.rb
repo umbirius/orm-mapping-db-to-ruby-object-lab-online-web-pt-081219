ab1db393e1907ccc00876331beabac4666b277dd
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-18 18:05:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,13 @@ class Student
     sql = <<-SQL
       SELECT * 
       FROM students 
-      WHERE grade = 9 
+      WHERE grade = 9
+    SQL
+    
+    DB[:conn].execute(sql, grade).map do |row|
+      self.new_from_db(row)
+    end 
+    
   end 
 
   def self.find_by_name(name)
